bb7a265e149c46f4a1b19b62e9761c40bd1e3e4b
karl <88538786+Karl-developer@users.noreply.github.com>
2022-08-09 19:00:27 -0300

    [FIX] fix responsive problems on cart; [FEAT] Add buttons on cart page

--- src/partials/HeroCart.jsx
+++ src/partials/HeroCart.jsx
@@ -4,11 +4,11 @@ import Button from '@mui/material/Button';
 function HeroCart() {
   return (
     <>
-    <div className="dark:bg-[#151617]">
+    <div className="dark:bg-[#151617] max-w-6xl">
       <button className="absolute dark:text-white text-black p-4">
         <Link to="/menu">Voltar</Link>
       </button>
-      <section className="flex flex-row items-center justify-center flex-wrap md:gap-12 -space-y-[300px] xs:space-y-0  min-h-screen">
+      <section className="flex flex-row items-center justify-center flex-wrap md:gap-12 -space-y-[300px] xs:space-y-0 pt-32">
         <div className="pr-4 pl-4">
           <img
             className="rounded-lg max-w-full h-auto"
@@ -16,20 +16,22 @@ function HeroCart() {
             alt=""
           />
         </div>
-        <div className="flex flex-col flex-wrap font-semibold font-inter">
+        <div className="flex flex-col flex-wrap font-semibold font-inter pt-64 xs:pt-0">
           <div className="border-t w-[150px] pt-3 border-gray-400">
             <ul className="space-y-2 dark:text-white text-black">
-              <li className="md:text-2xl text-xl">Produto:</li>
-              <li className="md:text-2xl text-xl">Preço:</li>
-              <li className="md:text-2xl text-xl">Descrição:</li>
+              <li className="md:text-2xl text-2xl whitespace-nowrap">Produto: amogus</li>
+              <li className="md:text-2xl text-2xl">Preço: 29,90</li>
+              <li className="md:text-2xl text-2xl whitespace-wrap">Descrição: <span className="text-sm">aoba aoba aoba aoba</span></li>
             </ul>
           </div>
         </div>
-        <div className="grid grid-cols-2 grid-flow-row">
-            <div><Button>sissu</Button></div>
-            <div><Button>aoba</Button></div>
-        </div>
       </section>
+      <div className="flex flex-wrap items-center justify-center flex-col">
+        <div className="grid grid-cols-2 grid-flow-row gap-4 pt-8 ">
+            <div><Button>Ver mais</Button></div>
+            <div><Button>Continuar</Button></div>
+        </div>
+        </div>
       </div>
     </>
   );
