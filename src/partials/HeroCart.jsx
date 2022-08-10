34011477eefa6a4e79ef5b68f767908d569e9196
karl <88538786+Karl-developer@users.noreply.github.com>
2022-08-10 17:48:59 -0300

    [FIX] Fix font size on cart page

--- src/partials/HeroCart.jsx
+++ src/partials/HeroCart.jsx
@@ -1,10 +1,18 @@
 import { Link } from "react-router-dom";
 import Button from '@mui/material/Button';
 
+
 function HeroCart() {
+
+  let response = {
+    produto : 'Disco do abba',
+    preco : 'R$ 100,00',
+    descricao : 'Disco do abba de 100GB',
+  }
+
   return (
     <>
-    <div className="dark:bg-[#151617] max-w-6xl">
+    <div className="dark:bg-[#151617] max-w-6xl xs:max-w-full min-h-screen">
       <button className="absolute dark:text-white text-black p-4">
         <Link to="/menu">Voltar</Link>
       </button>
@@ -16,20 +24,20 @@ function HeroCart() {
             alt=""
           />
         </div>
-        <div className="flex flex-col flex-wrap font-semibold font-inter pt-64 xs:pt-0">
+        <div className="flex flex-col flex-wrap xs:pr-0 pr-36 font-semibold font-inter pt-80 xs:pt-0">
           <div className="border-t w-[150px] pt-3 border-gray-400">
             <ul className="space-y-2 dark:text-white text-black">
-              <li className="md:text-2xl text-2xl whitespace-nowrap">Produto: amogus</li>
-              <li className="md:text-2xl text-2xl">Preço: 29,90</li>
-              <li className="md:text-2xl text-2xl whitespace-wrap">Descrição: <span className="text-sm">aoba aoba aoba aoba</span></li>
+              <li className="md:text-2xl text-2xl whitespace-nowrap">Produto: <span className="text-xl">{response.produto}</span></li>
+              <li className="md:text-2xl text-2xl whitespace-nowrap">Preço: <span className="text-xl">{response.preco}</span></li>
+              <li className="md:text-2xl text-2xl whitespace-wrap ">Descrição: <span className="text-sm">{response.descricao}</span></li>
             </ul>
           </div>
         </div>
       </section>
       <div className="flex flex-wrap items-center justify-center flex-col">
-        <div className="grid grid-cols-2 grid-flow-row gap-4 pt-8 ">
-            <div><Button>Ver mais</Button></div>
-            <div><Button>Continuar</Button></div>
+        <div className="grid grid-cols-2 grid-flow-row gap-4 pt-8 pb-8">
+            <div><Button variant='contained' size='medium'>Ver mais</Button></div>
+            <div><Button variant='contained' size='medium'>Continuar</Button></div>
         </div>
         </div>
       </div>
