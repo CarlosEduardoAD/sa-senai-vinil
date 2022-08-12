5224b1211605b2d5748a3b2197399e67c414dea7
karl <karl.devcontato@gmail.com>
2022-08-12 14:36:02 +0000

    [FIX] Fix sidebar button in menu page

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,11 +1,13 @@
 import AuthHeader from "../partials/AuthHeader";
 import HeroMenu from "../partials/HeroMenu";
 import Footer from "../partials/Footer"
+import {Sidebar} from "../utils/Sidebar"
 
 function Menu(){
     return(
         
         <section className="relative flex flex-col min-h-screen overflow-hidden">
+            <div className="z-auto"><Sidebar></Sidebar></div>
             <AuthHeader firstButton='Pedidos' secondButton='Carrinho' />
 
         <section>
