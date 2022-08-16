78fc3e2135323b9a2b458de0038cd406000f0f25
Luiz Felipe Warmling <luizfelipewarmling@gmail.com>
2022-08-16 19:46:26 -0300

    [NEW]: Added components for cart

---
+++ Front-end/src/components/CardsList/CardsList.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Card from '../Card/Card';
+import PropTypes from 'prop-types';
+import styles from './CardsList.module.scss';
+
+
+const CardsList = ({ cards, onClickHandler, changeFavouriteHandler, favouritesCardsArr }) => {
+    const cardsComponents = cards.map(({ title, price, articul, color, imgSrc }) => {
+        return (
+            <Card key={articul}
+                title={title}
+                price={price}
+                articul={articul}
+                color={color}
+                imgSrc={imgSrc}
+                isFavourite={favouritesCardsArr.includes(articul)}
+                onClickHandler={onClickHandler}
+                changeFavouriteHandler={changeFavouriteHandler} />
+        )
+    })
+
+    return (
+        <ul className={styles.list}>
+            {cardsComponents}
+        </ul>
+    );
+};
+
+CardsList.propTypes = {
+    cards: PropTypes.array.isRequired,
+    onClickHandler: PropTypes.func.isRequired,
+    changeFavouriteHandler: PropTypes.func.isRequired,
+    favouritesCardsArr: PropTypes.array
+};
+
+CardsList.defaultProps = {
+    cards: [],
+    favouritesCardsArr: []
+};
+
+export default CardsList;
