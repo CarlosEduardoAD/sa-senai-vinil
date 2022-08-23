fd2aa22c7d28110aa213add8d788717ccbead06e
karl <karl.devcontato@gmail.com>
2022-08-23 22:51:51 +0000

    [NEW] Add mariadb lib;

--- Back-end/src/index.ts
+++ Back-end/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
-import pg from 'pg'
 import { routes } from './routes'
+import mariadb from 'mariadb'
 
 const app = express();
 
@@ -10,5 +10,5 @@ app.use(express.json())
 app.use(routes)
 
 app.listen(5000 || 3000, () => {
-    console.log("Server is working ! ;)")
+    console.log('Server running...')
 })
