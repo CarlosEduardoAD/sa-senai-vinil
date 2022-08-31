c1fd537b7d607af2bcfd488cebafc80d6a79475c
LUIZ FELIPE WARMLING AMADEU <luiz_amadeu@estudante.sesisenai.org.br>
2022-08-31 12:25:18 -0300

    [FEAT]:Color button signs

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import styles from './Header.module.scss';
 import { NavLink, Link } from "react-router-dom";
 
-const Header = () => {
+function Header(props) {
+  const [top, setTop] = useState(true);
+  useEffect(() => {
+    const scrollHandler = () => {
+      window.pageYOffset > 10 ? setTop(false) : setTop(true);
+    };
+    window.addEventListener("scroll", scrollHandler);
+    return () => window.removeEventListener("scroll", scrollHandler);
+  }, [top]);
     return (
         <header className={styles.header}>
             <div className={styles.container}>
@@ -13,10 +21,10 @@ const Header = () => {
                         <li className={styles.item}><NavLink exact to="/Catalog" activeClassName={styles.active}>Produtos</NavLink></li>
                         <li className={styles.item}><NavLink exact to="/cart" activeClassName={styles.active}>Cart</NavLink></li>
                         <li className={styles.item}><NavLink exact to="/favourites" activeClassName={styles.active}>Favourites</NavLink></li>
-                        <li className={styles.item}><NavLink exact to="/SignIn" activeClassName={styles.active}>Sign In</NavLink></li>
-                        <li className={styles.item}><NavLink exact to="/SignUp" activeClassName={styles.active}>Sign Up</NavLink></li>                  
+                        <li className={styles.signin}><NavLink exact to="/SignIn" activeClassName={styles.active}>Sign In</NavLink></li>
+                        <li className={styles.signup}><NavLink exact to="/SignUp" activeClassName={styles.active}>Sign Up</NavLink></li>                  
                     </ul>
-                </nav>
+               </nav>
             </div>
         </header>
     );
