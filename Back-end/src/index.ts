927e4ac867492f947802cfe03ecd476585f97325
karl <karl.devcontato@gmail.com>
2022-08-31 15:05:25 +0000

    [NEW] new folder config

--- Back-end/src/index.ts
+++ Back-end/src/index.ts
@@ -9,6 +9,6 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3000, 'localhost', () => {
-    console.log('Server running...')
+app.listen(3000, 'localhost', async () => {
+    await console.log('Server running...')
 })
