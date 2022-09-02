e50a53e3a3189780ab8e0393878c70eeb6a2c2ed
karl <88538786+Karl-developer@users.noreply.github.com>
2022-09-01 21:34:28 -0300

    [FEAT] Implement bcrypt

--- Back-end/src/routes.ts
+++ Back-end/src/routes.ts
@@ -1,26 +1,35 @@
 import express from 'express'
 export const routes = express.Router()
 import mariadb from 'mariadb'
-import { user } from './user-interactions/register-user-interaction'
-import { userEmail } from './email-interactions/send-email-interaction'
+import { user } from './user-interactions/user-use-case'
+import { userHash } from './user-interactions/hash-interaction'
+import { userEmail } from './email-interactions/email-use-case'
+
+const conn = mariadb.createPool({
+    host : 'localhost',
+    user : 'root',
+    password : 'carloseduardo08',
+    database : 'goldies_sa'
+})
 
 routes.get('/', (req, res) => {
-    const conn = mariadb.createPool({
-        host : 'localhost',
-        user : 'root',
-        password : 'carloseduardo08',
-        database : 'goldies_sa'
-    })
     conn.getConnection().then(
         () => {res.json(req.body)}
     )
 })
 
 
-routes.post('/register', (req, res) => {{
+routes.post('/register', async (req, res) => {{
     const {nome , email, password} = req.body
-    let registerInteraction = new user(nome, email, password)
+    let obj = new userHash(password)
+    let result = obj.hashPassword()
+    let registerInteraction = new user(nome, email, await result)
     registerInteraction.registerUser()
     res.send('Deu certo a inserção no banco de bakas')
 }
+})
+
+routes.get('/login', (req, res) => {
+    conn.getConnection().then(() => {conn.query('SELECT * FROM usuario') .then(() => { res.json() })})
+
 })
